853d52c0ed34d7e84b9071ea17fe1ea04e3eb230
TS BIT <tomassag@gmail.com>
2021-07-09 14:55:05 +0300

    Sprendinys 01

--- js/main01.js
+++ js/main01.js
@@ -1,2 +1,17 @@
+
 const h1DOM = document.querySelector('h1');
+h1DOM.innerText = 'Labas vakaras!';
+// h1DOM.textContent = 'Labas vakaras!';
+console.log(h1DOM);
 // komentaras
+
+const pDOM = document.querySelector('p');
+pDOM.innerText = 'Šîandien gražus oras!';
+// pDOM.textContent = 'Šîandien gražus oras!';
+console.log(pDOM);
+
+const aDOM = document.querySelector('a');
+// aDOM.innerText = 'Spausk mane!';
+aDOM.textContent = 'Spausk mane!';
+console.log(aDOM);
+
