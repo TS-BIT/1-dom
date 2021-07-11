ef10d5f18a3e061b4176506da5fc3400ba1bc831
TS BIT <tomassag@gmail.com>
2021-07-11 16:58:39 +0300

    03 uzd spr final

--- js/main03.js
+++ js/main03.js
@@ -1,12 +1,40 @@
 const h1DOM = document.querySelector('h1');
 h1DOM.classList.add('simba');
+
 const allIngredientsDOM = document.querySelectorAll('li');
-// console.log(allIngredientsDOM);
+// allIngredientsDOM.classList.add('darzove');
+
+// console.log(allIngredientsDOM)
+// let array = [];
+
 for (let i = 0; i < allIngredientsDOM.length; i++) {
     const item = allIngredientsDOM[i];
-    const itemName = item.innerText;
+    item.classList.add('darzove');
+    // const itemName = item.innerText;
     // console.log(itemName);
     // const nameInUpperCase = itemName.toUpperCase();
     // console.log(nameInUpperCase);
-    allIngredientsDOM.classList.add('darzove')    
-}   
+    // array.push(nameInUpperCase);     
+}
+// console.log(array);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
