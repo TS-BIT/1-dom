eb135bf6cd37d38e6ee22a924c3d813ae77b550b
TS BIT <tomassag@gmail.com>
2021-07-14 08:12:31 +0300

    06 uzd srd final

--- js/main06.js
+++ js/main06.js
@@ -1,34 +1,26 @@
-const h1DOM = document.querySelector('.logo');
+const logoDOM = document.querySelector('.logo');
 const hamburgerDOM = document.querySelector('.hamburger');
+const navDOM = document.querySelector('nav');
 
+/*
 function hamburgerClick() {
-    // logoDOM.class.List.add('big');
-    // logoDOM.classList
-    // jei logo turi big klase
-        // tai nuimame
-    // jeigu neturi
-        // pridedame
-
     if (logoDOM.classList.contais('big')) {
         logoDOM.classList.remove('big');
     } else {
         logoDOM.classList.add('big');
-
-    }   
-}
+    }
+    if (navDOM.classList.contains('visible')) {
+        navDOM.classList.remove('visible');
+    } else {
+        navDOM.classList.add('visible');
+    }
+}   
 
 hamburgerDOM.addEventListener('click', hamburgerClick);
-
-
-/*
-function toggleLogo() {
-    logoDOM.classList.toggle('big');
-}
-
-// hamburgerDOM.addEventListener('click', hamburgerClick());
-h1DOM.innerText = 'Labas vakaras!';
-h1DOM.innerText = 'Labas rytas!';
-hamburgerDOM.addEventListener('click', toggleLogo);
 */
 
+hamburgerDOM.addEventListener('click', () => {
+    logoDOM.classList.toggle('big');
+    navDOM.classList.toggle('visible');
+  });
 
