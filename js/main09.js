c10493663b4148aaeaf40d84e5fbc25b567ad462
TS BIT <tomassag@gmail.com>
2021-07-18 08:00:10 +0300

    09 uzd spr

--- js/main09.js
+++ js/main09.js
@@ -5,26 +5,34 @@ const plusDOM = document.querySelector('.plus');
 const resetDOM = document.querySelector('.reset');
 const numberDOM = document.querySelector('.number');
 const h1DOM = document.querySelector('h1');
+const ulDOM = document.querySelector('ul');
+
+function htmlOnload() {
+    ulDOM.innerText = 'Tu pasileidai zaidima - sekmes!';
+    // console.log(ulDOM);
+}
+
 
 function minusClick() {
     numberDOM.innerText = --rezultatas;
-    h1DOM.innerText = 'Žaidimas progrese!';
-    console.log(h1DOM);
+    ulDOM.innerText = 'Paspaudei minusa ir dabartinis rezultatas yra .'; 
+    // console.log(ulDOM);
 }
     
 function plusClick() {
     numberDOM.innerText = ++rezultatas;
-    h1DOM.innerText = 'Žaidimas progrese!';
-    console.log(h1DOM);
+    ulDOM.innerText = 'Paspaudei pliusa ir dabartinis rezultatas yra .';
+    // console.log(ulDOM);
 }
 
 function resetClick() {
     rezultatas = 0;
     numberDOM.innerText = rezultatas;
-    h1DOM.innerText = 'Show must go on!';
-    console.log(h1DOM);      
+    ulDOM.innerText = 'Paspaudei reset ir dabartinis rezultatas yra 0.';
+    // console.log(ulDOM);      
 }
 
+ulDOM.addEventListener('onload', htmlOnload);
 minusDOM.addEventListener('click', minusClick);
 plusDOM.addEventListener('click', plusClick);
 resetDOM.addEventListener('click', resetClick);
